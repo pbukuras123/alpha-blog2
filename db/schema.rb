046decb7ec4a35e6776cc96224a8b64f2e2d4aee
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2020-05-29 19:00:27 -0400

    create article_categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_110520) do
+ActiveRecord::Schema.define(version: 2020_05_29_221456) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
