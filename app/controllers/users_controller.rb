c5810ef26b8020bfaacf3feead2129115c538f7e
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2020-05-29 06:41:03 -0400

    Update views and add user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update, :show]
+	before_action :set_user, only: [:edit, :update, :show, :destroy]
+	before_action :require_user, only: [:edit, :update, :destroy]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def show
 		@articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -16,6 +18,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:notice] = "Welcome to the the Alpha Blog #{@user.username}, you have successfully signed up"
 			redirect_to articles_path
 		else
@@ -36,6 +39,13 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def destroy
+		@user.destroy
+		session[:user_id] = nil if @user == current_user
+		flash[:notice] = "Account and all associated articles successfully deleted"
+		redirect_to articles_path
+	end
+
 	private
 
 	def user_params
@@ -44,6 +54,13 @@ class UsersController < ApplicationController
 
 	def set_user
 		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if current_user != @user && !current_user.admin?
+			flash[:alert] = "You can only edit or delete your own profile"
+			redirect_to @user
+		end
 	end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,5 @@ module ApplicationHelper
 		gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
 		image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto mt-4 d-block")
 	end
+
 end
